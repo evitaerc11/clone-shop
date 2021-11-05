ac6cd5a368c67c8b1e32a9c7cb20ec7b1ff35f48
evitaerc11 <abcdxyz14144@gmail.com>
2021-11-05 21:19:42 +0700

    start-backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import { Card, Col, Image, ListGroup, Row, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
-  const params = useParams();
-  const product = products.find((p) => p._id === params.id);
+  const { id } = useParams;
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [id, product]);
 
   return (
     <>
